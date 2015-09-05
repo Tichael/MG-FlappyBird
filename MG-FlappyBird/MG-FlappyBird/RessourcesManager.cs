4f9a6454df3ebe7ffee82fcc10e811fd21feeab9
Michael Tremblay <michael.tremblay11@hotmail.com>
2015-09-05 17:05:21 -0400

    Added Ressources Manager

---
+++ MG-FlappyBird/MG-FlappyBird/RessourcesManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MG_FlappyBird
+{
+    class RessourcesManager
+    {
+        // STATIC FIELDS
+        public static Texture2D sprite;
+
+        // LOAD CONTENT
+        public static void LoadContent(ContentManager content)
+        {
+            sprite = content.Load<Texture2D>("sprite");
+        }
+    }
+}
